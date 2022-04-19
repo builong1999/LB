fd45b0f56213cc3604d0ed6c29569b30b916fb0d
longbui-novobi <74339503+longbui-novobi@users.noreply.github.com>
2022-04-19 19:58:11 +0700

    update square

---
+++ square/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Square Logs',
+    'version': '1.6',
+    'category': 'Sales/CRM',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'application': True,
+    'auto_install': False,
+}
